e434331dcacc100af9d3871f49e0b1021b2fa467
Reshin311 <311reshin@gmail.com>
2021-01-21 22:01:04 +0900

    アカウントでログイン

--- LoginWithFiewbaseApp/HomeViewController.swift
+++ LoginWithFiewbaseApp/HomeViewController.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import UIKit
+import Firebase
+
+class HomeViewController: UIViewController {
+    
+    var user: User? {
+        didSet {
+            print("user?.name: ",user?.name)
+        }
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    
+    @IBAction func tappedLogoutButton(_ sender: Any) {
+       handleLogout()
+    }
+    
+    private func handleLogout() {
+        do {
+            try Auth.auth().signOut()
+            dismiss(animated: true, completion: nil)
+            //          presentToMainViewController()
+        }  catch (let err) {
+            print("ログアウトに失敗しました。: \(err)")
+            
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        logoutButton.layer.cornerRadius = 10
+
+        if let user = user {
+            
+        nameLabel.text = user.name + "さんようこそ"
+        emailLabel.text = user.email
+            
+        let dateString = dateFormatterForCreatedAt(date: user.createdAt.dateValue())
+        dateLabel.text = "作成日: " + dateString
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        confirmLoggedInUser()
+    }
+    
+    private func confirmLoggedInUser() {
+        if Auth.auth().currentUser?.uid == nil || user == nil {
+           presentToMainViewController()
+         
+       //     let navController = UINavigationController(rootViewController: viewController)
+       //     navController.modalPresentationStyle = .fullScreen
+            
+        
+        }
+    }
+    
+    
+      private func presentToMainViewController() {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(identifier: "ViewController") as! ViewController
+    //    viewController.modalPresentationStyle = .fullScreen
+        let navController = UINavigationController(rootViewController: viewController)
+       navController.modalPresentationStyle = .fullScreen
+        self.present(navController, animated: true, completion: nil)
+        
+        
+    }
+    
+
+    
+    private func dateFormatterForCreatedAt(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .long
+        formatter.locale = Locale(identifier: "jp_Jp")
+        return formatter.string(from: date)
+     
+    }
+        
+    }
+
+
+
--- LoginWithFiewbaseApp/ViewController.swift
+++ LoginWithFiewbaseApp/ViewController.swift
@@ -1,19 +1,202 @@
 //
 //  ViewController.swift
-//  LoginWithFiewbaseApp
+//  LoginWithFirebaseApp
 //
 //  Created by Shin on 2021/01/15.
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import PKHUD
+
+struct User {
+    let name: String
+    let createdAt: Timestamp
+    let email: String
+    
+    init(dic: [String: Any]) {
+        self.name = dic["name"] as! String
+        self.createdAt = dic["createdAt"] as! Timestamp
+        self.email = dic["email"] as! String
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+
+    @IBAction func tappedRegisterButton(_ sender: Any) {
+        handleAuthToFirebase()
+            
+        }
+    
+    @IBAction func tappedAlreadyHaveAccountButton(_ sender: Any) {
+        let storyBoard = UIStoryboard(name: "Login", bundle: nil)
+        let homeViewController = storyBoard.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        navigationController?.pushViewController(homeViewController, animated: true)
+ //       self.present(homeViewController, animated: true, completion: nil)
+        }
+    
+    
+    
+        private func handleAuthToFirebase() {
+            HUD.show(.progress, onView: view)
+            guard let email = emailTextField.text else { return }
+            guard let password = passwordTextField.text else { return }
+         
+    
+            Auth.auth().createUser(withEmail: email, password: password) { [self] (res,err) in
+        
+             if let err = err {
+             
+              print("認証情報の保存に失敗しました。\(err)")
+                HUD.hide { (_) in
+                HUD.flash(.error, delay: 1)
+                }
+                return
+       }
+                self.addUserInfoToFirestore(email: email)
+        
+            }
+        }
+   
+       private func addUserInfoToFirestore(email: String) {
+        guard let uid = Auth.auth().currentUser?.uid else { return}
+        guard let name = usernameTextField.text else { return }
+        
+        let docDate = ["email": email, "name": name, "createdAt": Timestamp()] as [String : Any]
+        let userRef = Firestore.firestore().collection("users").document(uid)
+        
+        userRef.setData(docDate) { (err) in
+                    if let err = err {
+                        
+                        print("Firestoreへの保存に失敗しました。\(err)" )
+                        HUD.hide { (_) in
+                        HUD.flash(.error, delay: 1)
+                        }
+                        return
+
+                    }
+            print("Firestoreへの保存に成功しました。" )
+           
+            
+
+            userRef.getDocument { (snapshot, err) in
+                if let err = err {
+                    print("ユーザー情報の取得に失敗しました。\(err)")
+                    HUD.hide { (_) in
+                    HUD.flash(.error, delay: 1)
+                    }
+                        return
+                }
+                       
+                guard let data = snapshot?.data() else { return }
+                let user = User.init(dic: data)
+                
+                print("ユーザー情報の取得ができました。\(user.name)")
+                HUD.hide { (_) in
+    //          HUD.flash(.success, delay: 1)
+                HUD.flash(.success, onView: self.view, delay: 1) { (_) in
+                        self.presentToHomeViewController(user: user)
+                        
+                    }
+                }
+            }
+    
+        }
+        
+   }
+    private func presentToHomeViewController(user: User) {
+                    let storyBoard = UIStoryboard(name: "Home", bundle: nil)
+                    let homeViewController = storyBoard.instantiateViewController(identifier: "HomeViewController") as! HomeViewController
+                    homeViewController.user = user
+                    homeViewController.modalPresentationStyle = .fullScreen
+                    self.present(homeViewController, animated: true, completion: nil)
+                
+                  }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        registerButton.isEnabled = false
+        registerButton.layer.cornerRadius = 10
+        registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.delegate = self
+        
+   //     var ref: DatabaseReference!
+   //     ref = Database.database().reference()
+        
+  //MARK: - keyboard
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(showKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(hideKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    
+    
 
+    @objc func showKeyboard(notification: Notification) {
+        let keyboardFrame = (notification.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as AnyObject).cgRectValue
+        
+        guard let keyboardMinY = keyboardFrame?.minY else { return }
+        let registerButtonMaxY = registerButton.frame.maxY
+        let distance = registerButtonMaxY - keyboardMinY + 20
+        
+        let transform = CGAffineTransform(translationX: 0, y: -distance)
+         
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [], animations: { self.view.transform = transform
+        })
+        
+//        print ("keyboardMinY : ",keyboardMinY, "registerButtonMaxY: ",registerButtonMaxY)
+    }
+
+    @objc func hideKeyboard() {
+          UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: [], animations: { self.view.transform = .identity
+        })
+    
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        let emailIsEmpty = emailTextField.text?.isEmpty ?? true
+        let passwordIsEmpty = passwordTextField.text?.isEmpty ?? true
+        let usernameIsEmpty = usernameTextField.text?.isEmpty ?? true
+        
+        if emailIsEmpty || passwordIsEmpty || usernameIsEmpty {
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+        } else {
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = UIColor.rgb(red: 255, green: 141, blue: 0)
+        }
+        
+    
+        
+    }
+    
+}
